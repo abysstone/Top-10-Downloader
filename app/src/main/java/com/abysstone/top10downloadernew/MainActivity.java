7a41713647aaca8d7325256cd4cf961175412121
abbystone <manasbhatt007@gmail.com>
2019-10-08 19:50:47 +0530

    with menu

--- app/src/main/java/com/abysstone/top10downloadernew/MainActivity.java
+++ app/src/main/java/com/abysstone/top10downloadernew/MainActivity.java
@@ -3,8 +3,11 @@ package com.abysstone.top10downloadernew;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.BufferedReader;
@@ -18,16 +21,62 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private ListView listApps;
 
+    private String feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+    private int feedLimit = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listApps=(ListView) findViewById(R.id.xmlListView);
 
-        Log.d(TAG, "onCreate: starting Asynctask");
+//        downloadUrl("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
+
+        downloadUrl(String.format(feedUrl,feedLimit));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.feed_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        //String feedUrl;
+
+        switch (id){
+            case R.id.mnufree:
+                feedUrl="http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+                break;
+            case R.id.mnuPaid:
+                feedUrl="http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml";
+                break;
+            case R.id.mnuSongs:
+                feedUrl="http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml";
+                break;
+            case R.id.mnu10:
+            case R.id.mnu25:
+                if (!item.isChecked()){
+                    item.setChecked(true);
+                    feedLimit = 35 - feedLimit;
+                    Log.d(TAG, "onOptionsItemSelected: "+ item.getTitle() + " setting feedLimit to " + feedLimit);
+                }else {
+                    Log.d(TAG, "onOptionsItemSelected: " + item.getTitle() + " setting feedLimit to " + feedLimit);
+                }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        downloadUrl(String.format(feedUrl,feedLimit));
+        return true;
+    }
+
+    private void downloadUrl(String feedUrl){
+        Log.d(TAG, "downloadUrl: starting Asynctask");
         DownloadData downloadData = new DownloadData();
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
-        Log.d(TAG, "onCreate: done");
+        downloadData.execute(feedUrl);
+        Log.d(TAG, "downloadUrl: done");
 
     }
 
@@ -37,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.d(TAG, "onPostExecute: parameter is " + s);
+//            Log.d(TAG, "onPostExecute: parameter is " + s);
             ParseApplications parseApplications = new ParseApplications();
             parseApplications.parse(s);
 
@@ -46,6 +95,8 @@ public class MainActivity extends AppCompatActivity {
             FeedAdapter feedAdapter = new FeedAdapter(MainActivity.this,R.layout.list_record,parseApplications.getApplications());
             listApps.setAdapter(feedAdapter);
         }
+
+
 
         @Override
         protected String doInBackground(String... strings) {
@@ -97,21 +148,3 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
